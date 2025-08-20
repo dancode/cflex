cc4895ef57f26169979a8736fb01c5721a5ea916
dancode <dancode@gmail.com>
2025-08-20 04:02:06 -0700

    Updated CMakeLists.xt

--- cflex/src/cflex/internal/cflex_parser.h
+++
@@ -1,5 +0,0 @@
-// This file will contain the parsing logic for the cflex_build tool.
-// It will be responsible for reading C source files and extracting reflection information.
-
-// Dummy declaration to prevent an empty translation unit warning.
-typedef int cflex_parser_dummy_t;
